9c4c76a0100f9162937124a84e394c2c6134c2ee
Zhang Yilin <yilinzh54@gmail.com>
2026-08-19 15:29:16 +0800

    Rename chatbot to Food

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Food User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Food.java
@@ -0,0 +1,11 @@
+public class Food {
+    public static void main(String[] args) {
+        String banner = "  _______  _______  _______  ______  \n"
+                + " |   ____||   __  ||   __  ||      \\ \n"
+                + " |  |___  |  |  | ||  |  | ||  ---  |\n"
+                + " |   ___| |  |  | ||  |  | ||  |  | |\n"
+                + " |  |     |  |__| ||  |__| ||  ---  |\n"
+                + " |__|     |_______||_______||______/ \n";
+        System.out.println(banner);
+    }
+}
